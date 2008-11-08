98ef38b5fe10760aaed358d78a743deea6e859a3
Matthew Ballance <matt.ballance@gmail.com>
2008-11-08 17:34:08 +0000

    Added C-style comment rule for the editor. Added initial version of partially-working SV parser

--- plugins/net.sf.sveditor.ui/src/net/sf/sveditor/ui/editor/SVPartitionScanner.java
+++ plugins/net.sf.sveditor.ui/src/net/sf/sveditor/ui/editor/SVPartitionScanner.java
@@ -26,45 +26,11 @@ public class SVPartitionScanner extends RuleBasedPartitionScanner {
 		
 		List<IPredicateRule> rules = new ArrayList<IPredicateRule>();
 		
-		rules.add(new MultiLineRule("/*", "*/", mlc, (char)0, true));
-		rules.add(new EndOfLineRule("//", Token.UNDEFINED));
+		rules.add(new CCommentRule(mlc));
+		rules.add(new EndOfLineRule("//", slc));
 		rules.add(new SingleLineRule("\"", "\"", Token.UNDEFINED, '\\'));
 		
-		// handle empty comments
-		// rules.add(new EmptyCommentPredicateRule(mlc));
-
-
 		IPredicateRule rulesArr[] = rules.toArray(new IPredicateRule[rules.size()]);
 		setPredicateRules(rulesArr);
 	}
-
-	private static class EmptyCommentDetector implements IWordDetector {
-		public boolean isWordStart(char c) {
-			return (c == '/');
-		}
-		
-		public boolean isWordPart(char c) {
-			return ((c == '*') || (c == '/'));
-		}
-	}
-	
-	private static class EmptyCommentPredicateRule extends WordRule implements IPredicateRule {
-		
-		private IToken			fSuccessToken;
-		
-		public EmptyCommentPredicateRule(IToken successToken) {
-			super(new EmptyCommentDetector());
-			fSuccessToken = successToken;
-			addWord("/**/", fSuccessToken);
-		}
-		
-		public IToken evaluate(ICharacterScanner scanner, boolean resume) {
-			return super.evaluate(scanner);
-		}
-		
-		public IToken getSuccessToken() {
-			return fSuccessToken;
-		}
-	}
-	
 }
