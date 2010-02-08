a91b3bfd720cd7b69fd66487d1495e91357f0722
Matthew Ballance <matt.ballance@gmail.com>
2010-02-08 01:46:39 +0000

    Update upload script to use one SFTP session with ignored-error commands rather than multiple sessions

--- scripts/upload_release.sh
+++ scripts/upload_release.sh
@@ -8,21 +8,11 @@ echo "Beginning release of $version" > release/release.log
 
 sftp_cmd="sftp -b /dev/stdin $SF_USERNAME,sveditor@frs.sourceforge.net"
 
-remote_mkdir() {
-    parent=`dirname $1`
-    dir=`basename $1`
-
-    $sftp_cmd << EOF
-       cd $parent
-       mkdir $dir
-EOF
-}
-
 # Copy the update site XML down for modification
 scp $SF_USERNAME,sveditor@web.sourceforge.net:htdocs/update/site.xml \
     release/site_ex.xml >> release/release.log 2>&1 
 
-if test $? -ne 0; then
+if test ! -f release/site_ex.xml; then
     echo "[ERROR] failed to download existing site.xml"
     exit 1
 fi
@@ -44,33 +34,35 @@ mkdir update_site
 
 sveditor_dir=/home/frs/project/s/sv/sveditor
 
-remote_mkdir $sveditor_dir/update_site
-remote_mkdir $sveditor_dir/update_site/$version
-remote_mkdir $sveditor_dir/update_site/$version/plugins
-remote_mkdir $sveditor_dir/update_site/$version/features
-
-remote_mkdir $sveditor_dir/sveditor/$version
-
-
 sftp -b /dev/stdin $SF_USERNAME,sveditor@frs.sourceforge.net << EOF
-cd $sveditor_dir
-cd update_site
+    cd $sveditor_dir
+    -mkdir update_site
+    cd update_site
+    -mkdir $version
+    cd $version
+    -mkdir plugins
+    -mkdir features
 
-cd $version/features
-put $update_site/features/net.sf.sveditor_${version}.jar
+    cd $sveditor_dir/sveditor
+    -mkdir $version
 
-cd ../plugins
-put $update_site/plugins/net.sf.sveditor.core_${version}.jar
-put $update_site/plugins/net.sf.sveditor.ui_${version}.jar
-put $update_site/plugins/net.sf.sveditor.libs.ovm_${version}.jar
+    cd $sveditor_dir/update_site
+    cd $version/features
+    put $update_site/features/net.sf.sveditor_${version}.jar
 
-cd ../../..
+    cd ../plugins
+    put $update_site/plugins/net.sf.sveditor.core_${version}.jar
+    put $update_site/plugins/net.sf.sveditor.ui_${version}.jar
+    put $update_site/plugins/net.sf.sveditor.libs.ovm_${version}.jar
 
-cd sveditor
-cd $version
-put sveditor-${version}.jar
-put sveditor-src-${version}.zip
-put release/rn.txt
+    cd ../../..
 
+    cd sveditor
+    cd $version
+    put sveditor-${version}.jar
+    put sveditor-src-${version}.zip
+    put release/rn.txt
 EOF
+
+
 
