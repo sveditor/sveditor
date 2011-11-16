e75792051a55f64ad4d80ca9e7cdc7ed07a29f64
Matthew Ballance <matt.ballance@gmail.com>
2011-11-16 15:30:58 +0000

    Correct copyright and minor fixes

--- plugins/net.sf.sveditor.ui/src/net/sf/sveditor/ui/editor/SVDoubleClickStrategy.java
+++ plugins/net.sf.sveditor.ui/src/net/sf/sveditor/ui/editor/SVDoubleClickStrategy.java
@@ -5,10 +5,17 @@
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  *
+ * Copyright (c) 2000, 2011 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
  * Contributors:
- *     Matthew Ballance - initial implementation
+ *     IBM Corporation - initial API and implementation
+ *     Matthew Ballance - customizations for SystemVerilog 
+ *     
  ****************************************************************************/
-
 
 package net.sf.sveditor.ui.editor;
 
