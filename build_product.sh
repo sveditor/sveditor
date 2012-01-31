949d0a29c243bf33097ae3af9c5d81e3b9b2f1d3
ballance <matt.ballance@gmail.com>
2012-01-31 08:55:52 -0800

    Add build_product script

---
+++ build_product.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+cd scripts
+
+$ECLIPSE_HOME/eclipse \
+    -nosplash -application org.eclipse.ant.core.antRunner \
+    --launcher.suppressErrors \
+    -buildfile mk_product.xml      \
+    -Dos=linux -Dws=gtk -Darch=x86_64 mk_product
+
