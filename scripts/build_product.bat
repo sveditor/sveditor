b43be4bfd7f9bc018381d227b2e8df3db5f294c6
unknown <ballance@ballance-PC.(none)>
2012-02-19 20:38:42 -0800

    Updates to the installer-build process

--- scripts/build_product.bat
+++ scripts/build_product.bat
@@ -1,5 +1,6 @@
 
 rem cd scripts
 
-%ECLIPSE_HOME%/eclipsec -nosplash -application org.eclipse.ant.core.antRunner --launcher.suppressErrors -buildfile mk_product.xml -Dos=win32 -Dws=win32 -Darch=x86 mk_product
+rem %ECLIPSE_HOME%/eclipsec -nosplash -application org.eclipse.ant.core.antRunner --launcher.suppressErrors -buildfile mk_product.xml -Dos=win32 -Dws=win32 -Darch=x86 mk_product
+%ECLIPSE_HOME%/eclipsec -nosplash -application org.eclipse.ant.core.antRunner --launcher.suppressErrors -buildfile mk_product.xml mk_product -Dos=win32
 
