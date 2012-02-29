e720438ac839d29e7a2d293896a9a4a28bf4c2eb
ballance <matt.ballance@gmail.com>
2012-02-28 20:41:49 -0800

    More changes for product build

--- scripts/build_product.sh
+++ scripts/build_product.sh
@@ -4,5 +4,5 @@ $ECLIPSE_HOME/eclipse \
     -nosplash -application org.eclipse.ant.core.antRunner \
     --launcher.suppressErrors \
     -buildfile mk_product.xml      \
-    -Dos=linux -Dws=gtk -Darch=x86_64 mk_product
+    -Dos=linux mk_product
 
