f3b7a24669b5394af29d26eb4cd1d3750df7a1f5
Armond Paiva <armondpaiva@gmail.com>
2012-05-27 12:10:27 -0500

    uvm1.0 basic tests revived

--- sveditor/plugins/net.sf.sveditor.core.tests/src/net/sf/sveditor/core/tests/index/TestUvmBasics.java
+++ sveditor/plugins/net.sf.sveditor.core.tests/src/net/sf/sveditor/core/tests/index/TestUvmBasics.java
@@ -14,22 +14,21 @@ package net.sf.sveditor.core.tests.index;
 
 import java.io.File;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 
 import junit.framework.TestCase;
 import net.sf.sveditor.core.SVCorePlugin;
+import net.sf.sveditor.core.SVFileUtils;
 import net.sf.sveditor.core.db.ISVDBItemBase;
 import net.sf.sveditor.core.db.SVDBItem;
 import net.sf.sveditor.core.db.SVDBItemType;
 import net.sf.sveditor.core.db.SVDBMarker;
 import net.sf.sveditor.core.db.SVDBMarker.MarkerType;
-import net.sf.sveditor.core.db.SVDBModIfcDecl;
 import net.sf.sveditor.core.db.index.ISVDBIndex;
 import net.sf.sveditor.core.db.index.ISVDBItemIterator;
 import net.sf.sveditor.core.db.index.SVDBArgFileIndexFactory;
-import net.sf.sveditor.core.db.index.SVDBIndexCollectionMgr;
 import net.sf.sveditor.core.db.index.SVDBIndexRegistry;
-import net.sf.sveditor.core.db.index.plugin_lib.SVDBPluginLibIndexFactory;
 import net.sf.sveditor.core.db.stmt.SVDBStmt;
 import net.sf.sveditor.core.db.stmt.SVDBVarDeclItem;
 import net.sf.sveditor.core.db.stmt.SVDBVarDeclStmt;
@@ -61,76 +60,200 @@ public class TestUvmBasics extends TestCase {
 			TestUtils.delete(fTmpDir);
 		}
 	}
+	
+	public void testBasicExamplePkg() {
+		
+		SVCorePlugin.getDefault().enableDebug(false);
+		
+		String test_name = "testBasicExamplePkg" ;
+		
+		File test_dir  = new File(fTmpDir,  test_name) ;
+		File proj_dir  = new File(test_dir, "uvm/examples/simple/basic_examples/pkg") ;
+		File uvm_dir   = new File(test_dir, "uvm") ;
+		File uvm_pkg   = new File(test_dir, "uvm/src/uvm_pkg.sv") ;
+		
+		StringBuilder list_file_conent = new StringBuilder();
+		
+		list_file_conent.append("+incdir+"+uvm_dir.toString()+"/src\n" +
+								uvm_pkg.toString()+"\n" +
+		                        "test.sv\n") ;		
+		
+		HashSet<String> requiredClasses = TestUtils.newHashSet("lower",
+				                                               "myunit",
+				                                               "myunit_wrapper",
+				                                               "mydata_wrapper") ;
+		
+		doTestUVMExample(test_name, 
+				test_dir, 
+				proj_dir, 
+				list_file_conent.toString(),
+				requiredClasses ) ;
 
-	public void testBasicProcessing() {
-		File tmpdir = new File(fTmpDir, "no_errors");
-		SVCorePlugin.getDefault().enableDebug(false);
-		LogHandle log = LogFactory.getLogHandle("testBasicProcessing");
-		
-		if (tmpdir.exists()) {
-			TestUtils.delete(tmpdir);
-		}
-		tmpdir.mkdirs();
-		
-		SVDBIndexRegistry rgy = SVCorePlugin.getDefault().getSVDBIndexRegistry();
-		rgy.init(TestIndexCacheFactory.instance(tmpdir));
+	}	
 	
-		SVDBIndexCollectionMgr index_mgr = new SVDBIndexCollectionMgr("GLOBAL");
-		index_mgr.addPluginLibrary(rgy.findCreateIndex(new NullProgressMonitor(),
-				"GLOBAL", "org.uvmworld.uvm", SVDBPluginLibIndexFactory.TYPE, null));
+	public void testBasicExampleEventPool() {
 		
-		ISVDBItemIterator index_it = index_mgr.getItemIterator(new NullProgressMonitor());
-		List<SVDBMarker> markers = new ArrayList<SVDBMarker>();
-		ISVDBItemBase ovm_component=null, ovm_sequence=null;
+		SVCorePlugin.getDefault().enableDebug(false);
 		
-		while (index_it.hasNext()) {
-			ISVDBItemBase it = index_it.nextItem();
-			log.debug("" + it.getType() + " " + SVDBItem.getName(it));
+		String test_name = "testBasicExampleEventPool" ;
+		
+		File test_dir  = new File(fTmpDir,  test_name) ;
+		File proj_dir  = new File(test_dir, "uvm/examples/simple/basic_examples/event_pool") ;
+		File uvm_dir   = new File(test_dir, "uvm") ;
+		File uvm_pkg   = new File(test_dir, "uvm/src/uvm_pkg.sv") ;
+		
+		StringBuilder list_file_conent = new StringBuilder();
+		
+		list_file_conent.append("+incdir+"+uvm_dir.toString()+"/src\n" +
+								uvm_pkg.toString()+"\n" +
+		                        "test.sv\n") ;		
+		
+		HashSet<String> requiredClasses = TestUtils.newHashSet() ;
+		
+		doTestUVMExample(test_name, 
+				test_dir, 
+				proj_dir, 
+				list_file_conent.toString(),
+				requiredClasses ) ;
+
+
+	}	
+	
+	public void testBasicExampleModule() {
+		
+		SVCorePlugin.getDefault().enableDebug(false);
+		
+		String test_name = "testBasicExampleModule" ;
+		
+		File test_dir  = new File(fTmpDir,  test_name) ;
+		File proj_dir  = new File(test_dir, "uvm/examples/simple/basic_examples/module") ;
+		File uvm_dir   = new File(test_dir, "uvm") ;
+		File uvm_pkg   = new File(test_dir, "uvm/src/uvm_pkg.sv") ;
+		
+		StringBuilder list_file_conent = new StringBuilder();
+		
+		list_file_conent.append("+incdir+"+uvm_dir.toString()+"/src\n" +
+								uvm_pkg.toString()+"\n" +
+		                        "test.sv\n") ;		
+		
+		HashSet<String> requiredClasses = TestUtils.newHashSet("lower","mydata") ;
+		
+		doTestUVMExample(test_name, 
+				test_dir, 
+				proj_dir, 
+				list_file_conent.toString(),
+				requiredClasses ) ;
+
+
+	}	
+	
+	public void testTrivial() {
+		
+		SVCorePlugin.getDefault().enableDebug(false);
+		
+		String test_name = "testTrivial" ;
+		
+		File test_dir  = new File(fTmpDir,  test_name) ;
+		File proj_dir  = new File(test_dir, "uvm/examples/simple/trivial") ;
+		File uvm_dir   = new File(test_dir, "uvm") ;
+		File uvm_pkg   = new File(test_dir, "uvm/src/uvm_pkg.sv") ;
+		
+		StringBuilder list_file_conent = new StringBuilder();
+		
+		list_file_conent.append("+incdir+"+uvm_dir.toString()+"/src\n" +
+								uvm_pkg.toString()+"\n" +
+		                        "component.sv\n") ;		
+		
+		HashSet<String> requiredClasses = TestUtils.newHashSet("my_component") ;
+		
+		doTestUVMExample(test_name, 
+				test_dir, 
+				proj_dir, 
+				list_file_conent.toString(),
+				requiredClasses ) ;
+
+
+	}				
+	
+	public void testSequenceBasicReadWrite() {
+		
+		SVCorePlugin.getDefault().enableDebug(false);
+		
+		String test_name = "testSequenceBasicReadWrite" ;
+		
+		File test_dir  = new File(fTmpDir,  test_name) ;
+		File proj_dir  = new File(test_dir, "uvm/examples/simple/sequence/basic_read_write_sequence") ;
+		File uvm_dir   = new File(test_dir, "uvm") ;
+		File uvm_pkg   = new File(test_dir, "uvm/src/uvm_pkg.sv") ;
+		
+		StringBuilder list_file_conent = new StringBuilder();
+		
+		list_file_conent.append("+incdir+"+uvm_dir.toString()+"/src\n" +
+								uvm_pkg.toString()+"\n" +
+		                        "top.sv\n") ;		
+		
+		HashSet<String> requiredClasses = TestUtils.newHashSet("bus_trans", "bus_rsp", "bus_req", "my_driver", "sequenceA", "env") ;
+		
+		doTestUVMExample(test_name, 
+				test_dir, 
+				proj_dir, 
+				list_file_conent.toString(),
+				requiredClasses ) ;
+
+	}				
+	
+	public void testInterfaces() {
+		
+		SVCorePlugin.getDefault().enableDebug(false);
+		
+		String test_name = "testInterfaces" ;
+		
+		File test_dir  = new File(fTmpDir,  test_name) ;
+		File proj_dir  = new File(test_dir, "uvm/examples/simple/interfaces") ;
+		File uvm_dir   = new File(test_dir, "uvm") ;
+		File uvm_pkg   = new File(test_dir, "uvm/src/uvm_pkg.sv") ;
+		
+		StringBuilder list_file_conent = new StringBuilder();
+		
+		list_file_conent.append("+incdir+"+uvm_dir.toString()+"/src\n" +
+								uvm_pkg.toString()+"\n" +
+		                        "interface.sv\n") ;		
+		
+		HashSet<String> requiredClasses = TestUtils.newHashSet("driver", "env") ;
+		
+		doTestUVMExample(test_name, 
+				test_dir, 
+				proj_dir, 
+				list_file_conent.toString(),
+				requiredClasses ) ;
+
+	}				
 			
-			if (it.getType() == SVDBItemType.Marker) {
-				markers.add((SVDBMarker)it);
-			} else if (it.getType() == SVDBItemType.ClassDecl) {
-				if (SVDBItem.getName(it).equals("uvm_component")) {
-					ovm_component = it;
-				} else if (SVDBItem.getName(it).equals("uvm_sequence")) {
-					ovm_sequence = it;
-				}
-			} else if (it.getType() == SVDBItemType.MacroDef) {
-			} else if (SVDBStmt.isType(it, SVDBItemType.VarDeclStmt)) {
-				SVDBVarDeclStmt v = (SVDBVarDeclStmt)it;
-				
-				SVDBVarDeclItem vi = (SVDBVarDeclItem)v.getChildren().iterator().next();
-				assertNotNull("Variable " + SVDBItem.getName(v.getParent()) + "." +
-						vi.getName() + " has a null TypeInfo", v.getTypeInfo());
-			}
-		}
+	public void doTestUVMExample(String 			testName, 
+								 File 				testDir, 
+								 File 				projDir, 
+								 String 			listFileContent, 
+								 HashSet<String> 	requiredClasses) {
 		
-		for (SVDBMarker m : markers) {
-			log.debug("[ERROR] " + m.getMessage());
-		}
-		
-		assertEquals("Check that no errors were found", 0, markers.size());
-		assertNotNull("Check found ovm_sequence", ovm_sequence);
-		assertNotNull("Check found ovm_component", ovm_component);
-		
-		LogFactory.removeLogHandle(log);
-	}
-	
-	public void testXbusExample() {
-		SVCorePlugin.getDefault().enableDebug(false);
 		BundleUtils utils = new BundleUtils(SVCoreTestsPlugin.getDefault().getBundle());
-		LogHandle log = LogFactory.getLogHandle("testXbusExample");
+		LogHandle log = LogFactory.getLogHandle(testName);
 		
-		File test_dir = new File(fTmpDir, "testXbusExample");
-		if (test_dir.exists()) {
-			TestUtils.delete(test_dir);
-		}
-		test_dir.mkdirs();
+		requiredClasses.add("uvm_component") ;
+		requiredClasses.add("uvm_sequence") ;
+		requiredClasses.add("uvm_object") ;
+		requiredClasses.add("uvm_sequencer") ;
+		requiredClasses.add("uvm_agent") ;
+		requiredClasses.add("uvm_transaction") ;
 		
-		utils.unpackBundleZipToFS("/uvm.zip", test_dir);		
-		File xbus = new File(test_dir, "uvm/examples/xbus");
+		testDir.mkdirs();
 		
-		IProject project_dir = TestUtils.createProject("xbus", xbus);
+		utils.unpackBundleZipToFS("/uvm.zip", testDir);		
+		
+		File listFile = new File(projDir, "file.list") ;
+		
+		IProject project = TestUtils.createProject(testName, projDir) ;
+		
+		SVFileUtils.writeToFile(listFile, listFileContent) ;
 		
 		File db = new File(fTmpDir, "db");
 		if (db.exists()) {
@@ -140,207 +263,53 @@ public class TestUvmBasics extends TestCase {
 		SVDBIndexRegistry rgy = SVCorePlugin.getDefault().getSVDBIndexRegistry();
 		rgy.init(TestIndexCacheFactory.instance(db));
 		
-		ISVDBIndex index = rgy.findCreateIndex(new NullProgressMonitor(), "GENERIC",
-				"${workspace_loc}/xbus/examples/compile_questa_sv.f",
+		ISVDBIndex index = 
+			rgy.findCreateIndex(new NullProgressMonitor(), "GENERIC",
+				listFile.toString(),
 				SVDBArgFileIndexFactory.TYPE, null);
 		
 		ISVDBItemIterator it = index.getItemIterator(new NullProgressMonitor());
 		List<SVDBMarker> errors = new ArrayList<SVDBMarker>();
 		
-		assertTrue("Cache is empty. No files found.",index.getCache().numFilesRead()!=0) ;
-		
 		while (it.hasNext()) {
-			ISVDBItemBase tmp_it = it.nextItem();
-			
-			if (tmp_it.getType() == SVDBItemType.Marker) {
-				SVDBMarker m = (SVDBMarker)tmp_it;
+			ISVDBItemBase item = it.nextItem();
+			if (item.getType() == SVDBItemType.Marker) {
+				SVDBMarker m = (SVDBMarker)item ;
 				if (m.getMarkerType() == MarkerType.Error) {
 					errors.add(m);
 				}
+			} else if (item.getType() == SVDBItemType.ClassDecl) {
+				String itemName = SVDBItem.getName(item) ;
+				if(requiredClasses.contains(itemName)) {
+					requiredClasses.remove(itemName) ;
+				}
+			} else if (SVDBStmt.isType(item, SVDBItemType.VarDeclStmt)) {
+				SVDBVarDeclStmt v = (SVDBVarDeclStmt)item;
+				SVDBVarDeclItem vi = (SVDBVarDeclItem)v.getChildren().iterator().next();
+				assertNotNull("Variable " + SVDBItem.getName(v.getParent()) + "." +
+						vi.getName() + " has a null TypeInfo", v.getTypeInfo());
 			}
-			
-			//log.debug("tmp_it=" + tmp_it.getName());
 		}
 		
 		for (SVDBMarker m : errors) {
-			log.debug("[ERROR] " + m.getMessage());
+			log.error("[ERROR] " + m.getMessage());
+		}
+		
+		assertEquals("Check that no errors were found", 0, errors.size());
+		
+		for(String className : requiredClasses) {
+			log.error("[ERROR] " + "Class \"" + className + "\" not found") ;
+		}
+		
+		assertTrue("Not all expected classes were parsed", requiredClasses.size()==0) ;
+		
+		for (SVDBMarker m : errors) {
+			log.error("[ERROR] " + m.getMessage());
 		}
 		assertEquals("No errors", 0, errors.size());
 		
 		LogFactory.removeLogHandle(log);
-		TestUtils.deleteProject(project_dir);
+		TestUtils.deleteProject(project);		
 	}
-
-	public void testTrivialExample() {
-		BundleUtils utils = new BundleUtils(SVCoreTestsPlugin.getDefault().getBundle());
-		LogHandle log = LogFactory.getLogHandle("testTrivialExample");
-		
-		File test_dir = new File(fTmpDir, "testTrivialExample");
-		if (test_dir.exists()) {
-			TestUtils.delete(test_dir);
-		}
-		test_dir.mkdirs();
-		
-		utils.unpackBundleZipToFS("/ovm.zip", test_dir);		
-		File trivial = new File(test_dir, "ovm/examples/trivial");
-		
-		IProject project_dir = TestUtils.createProject("trivial", trivial);
-		
-		File db = new File(fTmpDir, "db");
-		if (db.exists()) {
-			TestUtils.delete(db);
-		}
-		
-		SVDBIndexRegistry rgy = SVCorePlugin.getDefault().getSVDBIndexRegistry();
-		rgy.init(TestIndexCacheFactory.instance(db));
-		
-		ISVDBIndex index = rgy.findCreateIndex(new NullProgressMonitor(), 
-				"GENERIC", "${workspace_loc}/trivial/compile_questa_sv.f",
-				SVDBArgFileIndexFactory.TYPE, null);
-		
-		ISVDBItemIterator it = index.getItemIterator(new NullProgressMonitor());
-		List<SVDBMarker> errors = new ArrayList<SVDBMarker>();
-		
-		assertTrue("Cache is empty. No files found.",index.getCache().numFilesRead()!=0) ;
-		
-		while (it.hasNext()) {
-			ISVDBItemBase tmp_it = it.nextItem();
-			
-			if (tmp_it.getType() == SVDBItemType.Marker) {
-				SVDBMarker m = (SVDBMarker)tmp_it;
-				if (m.getMarkerType() == MarkerType.Error) {
-					errors.add(m);
-				}
-			}
-			
-			log.debug("tmp_it=" + SVDBItem.getName(tmp_it));
-		}
-		
-		for (SVDBMarker m : errors) {
-			log.debug("[ERROR] " + m.getMessage());
-		}
-		assertEquals("No errors", 0, errors.size());
-		LogFactory.removeLogHandle(log);
-		TestUtils.deleteProject(project_dir);
-	}
-	
-	public void testSequenceBasicReadWriteExample() {
-		BundleUtils utils = new BundleUtils(SVCoreTestsPlugin.getDefault().getBundle());
-		LogHandle log = LogFactory.getLogHandle("testSequenceBasicReadWriteExample");
-		
-		File test_dir = new File(fTmpDir, "testSequenceBasicReadWriteExample");
-		if (test_dir.exists()) {
-			TestUtils.delete(test_dir);
-		}
-		test_dir.mkdirs();
-		
-		utils.unpackBundleZipToFS("/ovm.zip", test_dir);		
-		File basic_read_write_sequence = new File(test_dir, "ovm/examples/sequence/basic_read_write_sequence");
-		
-		IProject project_dir = TestUtils.createProject("basic_read_write_sequence", basic_read_write_sequence);
-		
-		File db = new File(fTmpDir, "db");
-		if (db.exists()) {
-			TestUtils.delete(db);
-		}
-		
-		SVDBIndexRegistry rgy = SVCorePlugin.getDefault().getSVDBIndexRegistry();
-		rgy.init(TestIndexCacheFactory.instance(db));
-		
-		ISVDBIndex index = rgy.findCreateIndex(new NullProgressMonitor(), "GENERIC",
-				"${workspace_loc}/basic_read_write_sequence/compile_questa_sv.f",
-				SVDBArgFileIndexFactory.TYPE, null);
-		
-		ISVDBItemIterator it = index.getItemIterator(new NullProgressMonitor());
-		List<SVDBMarker> errors = new ArrayList<SVDBMarker>();
-		
-		SVDBModIfcDecl my_driver = null;
-		
-		while (it.hasNext()) {
-			ISVDBItemBase tmp_it = it.nextItem();
-			
-			if (tmp_it.getType() == SVDBItemType.Marker) {
-				SVDBMarker m = (SVDBMarker)tmp_it;
-				if (m.getMarkerType() == MarkerType.Error) {
-					errors.add(m);
-				}
-			} else if (tmp_it.getType() == SVDBItemType.ClassDecl &&
-					SVDBItem.getName(tmp_it).equals("my_driver")) {
-				my_driver = (SVDBModIfcDecl)tmp_it;
-			}
-			
-			//log.debug("tmp_it=" + tmp_it.getName());
-		}
-		
-		for (SVDBMarker m : errors) {
-			log.debug("[ERROR] " + m.getMessage());
-		}
-		assertEquals("No errors", 0, errors.size());
-		
-		assertNotNull(my_driver);
-		LogFactory.removeLogHandle(log);
-		
-		TestUtils.deleteProject(project_dir);
-	}
-
-	public void testSequenceSimpleExample() {
-		BundleUtils utils = new BundleUtils(SVCoreTestsPlugin.getDefault().getBundle());
-		LogHandle log = LogFactory.getLogHandle("testSequenceSimpleExample");
-		
-		File test_dir = new File(fTmpDir, "testSequenceSimpleExample");
-		if (test_dir.exists()) {
-			TestUtils.delete(test_dir);
-		}
-		test_dir.mkdirs();
-		
-		utils.unpackBundleZipToFS("/ovm.zip", test_dir);		
-		File simple = new File(test_dir, "ovm/examples/sequence/simple");
-		
-		IProject project_dir = TestUtils.createProject("simple", simple);
-		
-		File db = new File(fTmpDir, "db");
-		if (db.exists()) {
-			TestUtils.delete(db);
-		}
-		
-		SVDBIndexRegistry rgy = SVCorePlugin.getDefault().getSVDBIndexRegistry();
-		rgy.init(TestIndexCacheFactory.instance(db));
-		
-		ISVDBIndex index = rgy.findCreateIndex(new NullProgressMonitor(), 
-				"GENERIC","${workspace_loc}/simple/compile_questa_sv.f",
-				SVDBArgFileIndexFactory.TYPE, null);
-		
-		ISVDBItemIterator it = index.getItemIterator(new NullProgressMonitor());
-		List<SVDBMarker> errors = new ArrayList<SVDBMarker>();
-		
-		SVDBModIfcDecl simple_driver = null;
-		
-		while (it.hasNext()) {
-			ISVDBItemBase tmp_it = it.nextItem();
-			
-			if (tmp_it.getType() == SVDBItemType.Marker) {
-				SVDBMarker m = (SVDBMarker)tmp_it;
-				if (m.getMarkerType() == MarkerType.Error) {
-					errors.add(m);
-				}
-			} else if (tmp_it.getType() == SVDBItemType.ClassDecl &&
-					SVDBItem.getName(tmp_it).equals("simple_driver")) {
-				simple_driver = (SVDBModIfcDecl)tmp_it;
-			}
-			
-			//log.debug("tmp_it=" + tmp_it.getName());
-		}
-		
-		for (SVDBMarker m : errors) {
-			log.debug("[ERROR] " + m.getMessage());
-		}
-		assertEquals("No errors", 0, errors.size());
-		
-		assertNotNull(simple_driver);
-		LogFactory.removeLogHandle(log);
-		
-		TestUtils.deleteProject(project_dir);
-	}
-
 
 }
