caa4bfed76835e11721b8b0c1bac419089d78646
Armond Paiva <armondpaiva@gmail.com>
2012-06-03 21:37:07 -0500

    objects view tied to decl cache

--- sveditor/plugins/net.sf.sveditor.core/src/net/sf/sveditor/core/objects/ObjectsTreeFactory.java
+++ sveditor/plugins/net.sf.sveditor.core/src/net/sf/sveditor/core/objects/ObjectsTreeFactory.java
@@ -14,11 +14,19 @@
 package net.sf.sveditor.core.objects;
 
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+
+import org.eclipse.core.runtime.NullProgressMonitor;
 
 import net.sf.sveditor.core.db.SVDBItemType;
 import net.sf.sveditor.core.db.index.ISVDBIndex;
 import net.sf.sveditor.core.db.index.SVDBDeclCacheItem;
+import net.sf.sveditor.core.db.search.SVDBFindClassMatcher;
+import net.sf.sveditor.core.db.search.SVDBFindInterfaceMatcher;
+import net.sf.sveditor.core.db.search.SVDBFindModuleMatcher;
+import net.sf.sveditor.core.db.search.SVDBFindPackageMatcher;
 
 
 public class ObjectsTreeFactory {
@@ -34,6 +42,11 @@ public class ObjectsTreeFactory {
 			return null ;
 		}
 		
+		Map<String,SVDBDeclCacheItem> pkgMap = new HashMap<String, SVDBDeclCacheItem>();
+		Map<String,SVDBDeclCacheItem> globalPkgMap = new HashMap<String, SVDBDeclCacheItem>() ;
+		Map<String,SVDBDeclCacheItem> ifaceMap = new HashMap<String, SVDBDeclCacheItem>() ;
+		Map<String,SVDBDeclCacheItem> moduleMap = new HashMap<String, SVDBDeclCacheItem>() ;
+		
 		ObjectsTreeNode topNode  = new ObjectsTreeNode(null, "Top") ;
 		
 		// TODO: spin through the incoming projectIndexList and produce the real tree.
@@ -43,36 +56,97 @@ public class ObjectsTreeFactory {
 		topNode.addChild(packagesNode) ;
 		packagesNode.setItemDecl(new SVDBDeclCacheItem(null, null, ObjectsTreeNode.PACKAGES_NODE, SVDBItemType.PackageDecl)) ;
 		
-		for(String pkgName: Arrays.asList("axi_agent_pkg", "cpu_agent_pkg")) {
-			SVDBDeclCacheItem pkgDeclItem = new SVDBDeclCacheItem(null, null, pkgName, SVDBItemType.PackageDecl) ;
-			ObjectsTreeNode pkgNode = new ObjectsTreeNode(packagesNode, pkgName, pkgDeclItem) ;  
-			packagesNode.addChild(pkgNode) ; 
-			for(String className: Arrays.asList("bfm", "mon", "agent")) {
-				SVDBDeclCacheItem classDeclItem = new SVDBDeclCacheItem(null, null, className, SVDBItemType.ClassDecl) ;
-				ObjectsTreeNode classNode = new ObjectsTreeNode(pkgNode, className, classDeclItem); 
-				pkgNode.addChild(classNode) ; 
+		ObjectsTreeNode rootPkgNode = new ObjectsTreeNode(packagesNode, "root") ;
+		packagesNode.addChild(rootPkgNode) ;
+		rootPkgNode.setItemDecl(new SVDBDeclCacheItem(null, null, "root", SVDBItemType.PackageDecl)) ;
+		
+		// Global classes go into the "root" package
+		//
+		for(ISVDBIndex svdbIndex: fProjectIndexList) {
+			List<SVDBDeclCacheItem> rootClasses = svdbIndex.findGlobalScopeDecl(new NullProgressMonitor(), "rootClasses", new SVDBFindClassMatcher()) ;
+			if(rootClasses != null) {
+				for(SVDBDeclCacheItem rootClass: rootClasses) {
+					if(rootClass.getName().matches("^__.*")) { continue ; } // Skip builtins
+					if(!globalPkgMap.containsKey(rootClass.getName())) {
+						ObjectsTreeNode rootClassNode = new ObjectsTreeNode(rootPkgNode, rootClass.getName(), rootClass) ;  
+						rootPkgNode.addChild(rootClassNode) ; 
+						globalPkgMap.put(rootClass.getName(), rootClass) ;
+					}
+				}
 			}
 		}
+		
+		for(ISVDBIndex svdbIndex: fProjectIndexList) {
+			List<SVDBDeclCacheItem> packages = svdbIndex.findGlobalScopeDecl(new NullProgressMonitor(), "pkgs", new SVDBFindPackageMatcher()) ;
+			if(packages != null) {
+				for(SVDBDeclCacheItem pkg: packages) {
+					if(!pkgMap.containsKey(pkg.getName())) {
+						ObjectsTreeNode pkgNode = new ObjectsTreeNode(packagesNode, pkg.getName(), pkg) ;  
+						packagesNode.addChild(pkgNode) ; 
+						pkgMap.put(pkg.getName(), pkg) ;
+					}
+				}
+			}
+		}
+		
+//		for(String pkgName: Arrays.asList("axi_agent_pkg", "cpu_agent_pkg")) {
+//			SVDBDeclCacheItem pkgDeclItem = new SVDBDeclCacheItem(null, null, pkgName, SVDBItemType.PackageDecl) ;
+//			ObjectsTreeNode pkgNode = new ObjectsTreeNode(packagesNode, pkgName, pkgDeclItem) ;  
+//			packagesNode.addChild(pkgNode) ; 
+//			for(String className: Arrays.asList("bfm", "mon", "agent")) {
+//				SVDBDeclCacheItem classDeclItem = new SVDBDeclCacheItem(null, null, className, SVDBItemType.ClassDecl) ;
+//				ObjectsTreeNode classNode = new ObjectsTreeNode(pkgNode, className, classDeclItem); 
+//				pkgNode.addChild(classNode) ; 
+//			}
+//		}
 		
 		ObjectsTreeNode modulesNode = new ObjectsTreeNode(topNode, ObjectsTreeNode.MODULES_NODE) ;
 		topNode.addChild(modulesNode) ;
 		modulesNode.setItemDecl(new SVDBDeclCacheItem(null, null, ObjectsTreeNode.MODULES_NODE, SVDBItemType.ModuleDecl)) ;
 		
-		for(String pkgName: Arrays.asList("axi", "cpu")) {
-			SVDBDeclCacheItem moduleDeclItem = new SVDBDeclCacheItem(null, null, pkgName, SVDBItemType.ModuleDecl) ;
-			ObjectsTreeNode moduleNode = new ObjectsTreeNode(packagesNode, pkgName, moduleDeclItem) ;  
-			modulesNode.addChild(moduleNode) ; 
+		for(ISVDBIndex svdbIndex: fProjectIndexList) {
+			List<SVDBDeclCacheItem> modules = svdbIndex.findGlobalScopeDecl(new NullProgressMonitor(), "modules", new SVDBFindModuleMatcher()) ;
+			if(modules != null) {
+				for(SVDBDeclCacheItem module: modules) {
+					if(!moduleMap.containsKey(module.getName())) {
+						ObjectsTreeNode moduleNode = new ObjectsTreeNode(modulesNode, module.getName(), module) ;  
+						modulesNode.addChild(moduleNode) ; 
+						moduleMap.put(module.getName(),module) ;
+					}
+				}
+			}
 		}
+		
+		
+//		for(String pkgName: Arrays.asList("axi", "cpu")) {
+//			SVDBDeclCacheItem moduleDeclItem = new SVDBDeclCacheItem(null, null, pkgName, SVDBItemType.ModuleDecl) ;
+//			ObjectsTreeNode moduleNode = new ObjectsTreeNode(packagesNode, pkgName, moduleDeclItem) ;  
+//			modulesNode.addChild(moduleNode) ; 
+//		}
 		
 		ObjectsTreeNode interfacesNode = new ObjectsTreeNode(topNode, ObjectsTreeNode.INTERFACES_NODE) ;
 		topNode.addChild(interfacesNode) ;
 		interfacesNode.setItemDecl(new SVDBDeclCacheItem(null, null, ObjectsTreeNode.INTERFACES_NODE, SVDBItemType.InterfaceDecl)) ;
 		
-		for(String pkgName: Arrays.asList("axi", "cpu")) {
-			SVDBDeclCacheItem interfaceDeclItem = new SVDBDeclCacheItem(null, null, pkgName, SVDBItemType.InterfaceDecl) ;
-			ObjectsTreeNode moduleNode = new ObjectsTreeNode(packagesNode, pkgName, interfaceDeclItem) ;  
-			interfacesNode.addChild(moduleNode) ; 
+		for(ISVDBIndex svdbIndex: fProjectIndexList) {
+			List<SVDBDeclCacheItem> interfaces = svdbIndex.findGlobalScopeDecl(new NullProgressMonitor(), "interfaces", new SVDBFindInterfaceMatcher()) ;
+			if(interfaces != null) {
+				for(SVDBDeclCacheItem iface: interfaces) {
+					if(!ifaceMap.containsKey(iface.getName())) {
+						ObjectsTreeNode ifaceNode = new ObjectsTreeNode(interfacesNode, iface.getName(), iface) ;  
+						interfacesNode.addChild(ifaceNode) ; 
+						ifaceMap.put(iface.getName(),iface) ;
+					}
+				}
+			}
 		}
+		
+		
+//		for(String pkgName: Arrays.asList("axi", "cpu")) {
+//			SVDBDeclCacheItem interfaceDeclItem = new SVDBDeclCacheItem(null, null, pkgName, SVDBItemType.InterfaceDecl) ;
+//			ObjectsTreeNode moduleNode = new ObjectsTreeNode(packagesNode, pkgName, interfaceDeclItem) ;  
+//			interfacesNode.addChild(moduleNode) ; 
+//		}
 		
 		return topNode ;
 		
