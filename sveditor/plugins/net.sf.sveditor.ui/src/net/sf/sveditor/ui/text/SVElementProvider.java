ece9fde7b0d432f00a462f07be12a3a99aec1e84
Armond Paiva <armondpaiva@gmail.com>
2012-06-09 17:18:08 -0500

    quick hierarchy add

--- sveditor/plugins/net.sf.sveditor.ui/src/net/sf/sveditor/ui/text/SVElementProvider.java
+++ sveditor/plugins/net.sf.sveditor.ui/src/net/sf/sveditor/ui/text/SVElementProvider.java
@@ -13,11 +13,9 @@
 
 package net.sf.sveditor.ui.text;
 
-//public class SVElementProvider {
-//
-//}
-
+import net.sf.sveditor.core.db.ISVDBItemBase;
 import net.sf.sveditor.ui.editor.SVEditor;
+import net.sf.sveditor.ui.editor.actions.SelectionConverter;
 
 import org.eclipse.jface.text.IRegion;
 import org.eclipse.jface.text.ITextViewer;
@@ -27,31 +25,16 @@ import org.eclipse.jface.text.information.IInformationProviderExtension;
 
 import org.eclipse.ui.IEditorPart;
 
-//import org.eclipse.jdt.core.IJavaElement;
-//import org.eclipse.jdt.core.ITypeParameter;
-//import org.eclipse.jdt.core.JavaModelException;
-//
-//import org.eclipse.jdt.internal.ui.actions.SelectionConverter;
-//import org.eclipse.jdt.internal.ui.javaeditor.EditorUtility;
-//import org.eclipse.jdt.internal.ui.javaeditor.JavaEditor;
-
 /**
  * Provides a Java element to be displayed in by an information presenter.
  */
 public class SVElementProvider implements IInformationProvider, IInformationProviderExtension {
 
 	private SVEditor fEditor;
-	private boolean fUseCodeResolve;
 
 	public SVElementProvider(IEditorPart editor) {
-		fUseCodeResolve= false;
 		if (editor instanceof SVEditor)
 			fEditor= (SVEditor)editor;
-	}
-
-	public SVElementProvider(IEditorPart editor, boolean useCodeResolve) {
-		this(editor);
-		fUseCodeResolve= useCodeResolve;
 	}
 
 	/*
@@ -60,11 +43,11 @@ public class SVElementProvider implements IInformationProvider, IInformationProv
 	public IRegion getSubject(ITextViewer textViewer, int offset) {
 		if (textViewer != null && fEditor != null) {
 //			IRegion region= JavaWordFinder.findWord(textViewer.getDocument(), offset);
-			IRegion region=null ; // FIXME:
-			if (region != null)
-				return region;
-			else
-				return new Region(offset, 0);
+//			IRegion region=null ; // FIXME:
+//			if (region != null)
+//				return region;
+//			else
+			return new Region(offset, 0);
 		}
 		return null;
 	}
@@ -80,28 +63,12 @@ public class SVElementProvider implements IInformationProvider, IInformationProv
 	 * @see IInformationProviderExtension#getElement(ITextViewer, IRegion)
 	 */
 	public Object getInformation2(ITextViewer textViewer, IRegion subject) {
-//		if (fEditor == null)
-//			return null;
-//
-//		try {
-//			if (fUseCodeResolve) {
-//				IStructuredSelection sel= SelectionConverter.getStructuredSelection(fEditor);
-//				if (!sel.isEmpty()) {
-//					Object element= sel.getFirstElement();
-//					if (!(element instanceof ITypeParameter))
-//						return element;
-//				}
-//			}
-//			IJavaElement element= SelectionConverter.getElementAtOffset(fEditor, false);
-//			if (element != null)
-//				return element;
-//
-//			return EditorUtility.getEditorInputJavaElement(fEditor, false);
-//		} catch (JavaModelException e) {
-//			return null;
-//		}
+		if (fEditor == null)
+			return null;
 		
-//		return null ;
-		return fEditor ; // FIXME: just want to return !null for now
+		ISVDBItemBase element = SelectionConverter.getElementAtOffset(fEditor) ;
+		
+		return element ;
 	}
+	
 }
