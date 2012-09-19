bada0fe662232a4770102338fcc4b9f7327a24e1
mballance <matt.ballance@gmail.com>
2012-09-19 08:57:54 -0700

    Content assist bug fixes for qualified references to types/enumerators declared in packages and classes

--- sveditor/plugins/net.sf.sveditor.ui/src/net/sf/sveditor/ui/argfile/editor/SVArgFileEditor.java
+++ sveditor/plugins/net.sf.sveditor.ui/src/net/sf/sveditor/ui/argfile/editor/SVArgFileEditor.java
@@ -1,6 +1,10 @@
 package net.sf.sveditor.ui.argfile.editor;
 
+import java.util.ResourceBundle;
+
 import net.sf.sveditor.core.log.ILogLevel;
+import net.sf.sveditor.ui.SVUiPlugin;
+import net.sf.sveditor.ui.argfile.editor.actions.OpenDeclarationAction;
 
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.ui.editors.text.TextEditor;
@@ -16,6 +20,27 @@ public class SVArgFileEditor extends TextEditor implements ILogLevel {
 	
 	public SVArgFileCodeScanner getCodeScanner() {
 		return fCodeScanner;
+	}
+	
+
+	@Override
+	protected void createActions() {
+		super.createActions();
+		
+		ResourceBundle bundle = SVUiPlugin.getDefault().getResources();
+
+		OpenDeclarationAction od_action = new OpenDeclarationAction(bundle, this);
+		od_action.setActionDefinitionId(SVUiPlugin.PLUGIN_ID + ".editor.open.declaration");
+		setAction(SVUiPlugin.PLUGIN_ID + ".svOpenEditorAction", od_action);
+		markAsStateDependentAction(SVUiPlugin.PLUGIN_ID + ".svOpenEditorAction", false);
+		markAsSelectionDependentAction(SVUiPlugin.PLUGIN_ID + ".svOpenEditorAction", false);
+	}
+
+
+	@Override
+	protected void initializeKeyBindingScopes() {
+		// TODO Auto-generated method stub
+		super.initializeKeyBindingScopes();
 	}
 
 
