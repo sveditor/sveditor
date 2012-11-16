11b280b9c57fa646e28be7295796a1f3eea264db
stevenaz <steven.dawson.az@gmail.com>
2012-11-15 21:40:40 -0700

    Fixed issue with auto-indenting, this has to do with a begin at the start of the line, need to un-indent it

--- sveditor/plugins/net.sf.sveditor.core/src/net/sf/sveditor/core/indent/SVDefaultIndenter2.java
+++ sveditor/plugins/net.sf.sveditor.core/src/net/sf/sveditor/core/indent/SVDefaultIndenter2.java
@@ -280,7 +280,7 @@ public class SVDefaultIndenter2 implements ISVIndenter {
 			boolean begin_is_start_line = tok.isStartLine();
 			
 			if (begin_is_start_line) {
-				enter_scope(tok);
+				leave_scope(tok);		// un-indent, we already indented before we came here, assuming it was a single-line bit of code
 				start_of_scope(tok);
 			}
 			
@@ -297,18 +297,10 @@ public class SVDefaultIndenter2 implements ISVIndenter {
 				}
 				if (tok.isId("end")) {
 					leave_scope(tok);
-					if (begin_is_start_line) {
-						leave_scope();
-					}
 					if (fDebugEn) {
 						debug("Setting indent \"" + peek_indent() + "\"");
 					}
 					tok = next_s();
-					// TODO: Why does setting indent here cause 
-					// new-line positioning of 'begin' to work properly?
-					if (begin_is_start_line) {
-						set_indent(tok, false);
-					}
 					
 					tok = consume_labeled_block(tok);
 					break;
