99da6fd5b602bcbe5825e5cb21fecc4126aeeb61
mballance-sf <matt.ballance@gmail.com>
2013-01-31 18:50:08 -0800

    Add support for SV file extensions, more work on documentation, eliminate resource exceptions from the file decorators

--- web/mkweb.sh
+++ web/mkweb.sh
@@ -3,7 +3,7 @@
 $ECLIPSE_HOME/eclipse \
     -nosplash -application org.eclipse.ant.core.antRunner \
     --launcher.suppressErrors \
-    -buildfile mkdoc.xml      \
+    -buildfile mkweb.xml      \
     -verbose \
-    -Dos=linux -Dws=gtk -Darch=x86_64 $extra_defs mk_user_guide_pdf
+    -Dos=linux -Dws=gtk -Darch=x86_64 $extra_defs mk_html
 
