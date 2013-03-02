0f31b02ede97b1bfaf54dd159944d04b64a84912
stevenaz <steven.dawson.az@gmail.com>
2013-03-01 17:17:56 -0700

    Forgot the hex characters ... idiot.

--- sveditor/plugins/net.sf.sveditor.ui/src/net/sf/sveditor/ui/editor/SVCodeScanner.java
+++ sveditor/plugins/net.sf.sveditor.ui/src/net/sf/sveditor/ui/editor/SVCodeScanner.java
@@ -149,7 +149,7 @@ public class SVCodeScanner extends RuleBasedScanner {
 		rules.add(new WordRule(new IWordDetector() {
 			
 			public boolean isWordPart(char c) {
-				final char fBraceStrings[] = { '0','1','2','3','4','5','6','7','8','9','\'','h','d','b','o','_'};
+				final char fBraceStrings[] = { '0','1','2','3','4','5','6','7','8','9','\'','h','d','b','o','_','a','b','c','d','e','f','A','B','C','D','E','F'};
 				for (char ch : fBraceStrings)  {
 					if (ch == c)
 						return true;
