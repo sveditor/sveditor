a4436656ce06e78640e94b5529007fd652a479dd
mballance <matt.ballance@gmail.com>
2013-10-09 21:31:47 -0700

    Updates to installers

---
+++ scripts/mk_deb.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+is_mingw=`uname | sed -e 's/MINGW.*$/1/'`
+
+if test "$is_mingw" = "1"; then
+  os=win32
+  ws=win32
+  arch=x86_64
+  eclipse=eclipsec
+else
+  os=linux
+  ws=gtk
+  if test `uname -m` = "x86_64"; then
+    arch=x86_64
+  else
+    arch=x86
+  fi
+  eclipse=eclipse
+fi
+
+$ECLIPSE_HOME/${eclipse} \
+    -nosplash -application org.eclipse.ant.core.antRunner \
+    --launcher.suppressErrors \
+    -buildfile mk_sve.xml mk_deb
+
