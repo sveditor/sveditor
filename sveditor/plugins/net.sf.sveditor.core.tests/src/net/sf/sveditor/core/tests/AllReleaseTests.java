9ef1796a335535712e1d113c894daea240695f3a
mballance <matt.ballance@gmail.com>
2014-11-25 19:09:42 -0800

    Updates to better-support modular release tests

--- sveditor/plugins/net.sf.sveditor.core.tests/src/net/sf/sveditor/core/tests/AllReleaseTests.java
+++ sveditor/plugins/net.sf.sveditor.core.tests/src/net/sf/sveditor/core/tests/AllReleaseTests.java
@@ -10,17 +10,41 @@
  ****************************************************************************/
 
 
-package net.sf.sveditor.ui.tests;
+package net.sf.sveditor.core.tests;
 
 import junit.framework.Test;
+import junit.framework.TestCase;
 import junit.framework.TestSuite;
-import net.sf.sveditor.core.tests.CoreReleaseTests;
+
+import org.eclipse.core.runtime.IConfigurationElement;
+import org.eclipse.core.runtime.IExtension;
+import org.eclipse.core.runtime.IExtensionPoint;
+import org.eclipse.core.runtime.IExtensionRegistry;
+import org.eclipse.core.runtime.Platform;
 
 public class AllReleaseTests extends TestSuite {
 	
 	public AllReleaseTests() {
-		addTest(CoreReleaseTests.suite());
-		addTest(UiReleaseTests.suite());
+		IExtensionRegistry e_rgy = Platform.getExtensionRegistry();
+		
+		IExtensionPoint ep = e_rgy.getExtensionPoint(
+				"net.sf.sveditor.core.tests", "releaseTests");
+		
+		for (IExtension ext : ep.getExtensions()) {
+			for (IConfigurationElement el : ext.getConfigurationElements()) {
+				Object suite_o = null;
+				try {
+					suite_o = el.createExecutableExtension("class");
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+				
+				TestSuite suite = (TestSuite)suite_o;
+				addTest(suite);
+			}
+		}
+//		addTest(CoreReleaseTests.suite());
+//		addTest(UiReleaseTests.suite());
 	}
 	
 	public static Test suite() {
