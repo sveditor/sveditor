102b811fe19fb5863546a4a0f41bdcb8c5045862
stevenaz <steven.dawson.az@gmail.com>
2015-03-26 23:28:23 -0700

    #374 Specify parsing +=> - Found additional operators that weren't being handled correctly

--- sveditor/plugins/net.sf.sveditor.core.tests/src/net/sf/sveditor/core/tests/parser/TestParseSpecify.java
+++ sveditor/plugins/net.sf.sveditor.core.tests/src/net/sf/sveditor/core/tests/parser/TestParseSpecify.java
@@ -26,20 +26,23 @@ public class TestParseSpecify extends TestCase {
 	public void testSpecifyBlock() throws SVParseException {
 		String testname = "testSpecifyBlock";
 		String doc = 
-				"module delay (in, out);\n" +
-				" input  in;\n" +
-				" output out;\n" +
-				"\n" +
-				" assign out = in;\n" +
-				"\n" +
-				" specify\n" +
-				" (in => out) = (600,600);\n" +
-				" (in +=> out) = (600,600);\n" +
-				" (in -=> out) = (600,600);\n" +
-				" (in1,in2  *> out      ) = (600,600); // multi-to-single\n" +
-				" (in      -*> out1,out2) = (600,600); // single-to-multi\n" +
-				" (in1,in2 +*> out1,out2) = (600,600); //multi-to-multi\n" +
-				" endspecify\n" +
+				"module delay (in, in1, in2, out, out1, out2);\n" +
+				"	input  in, in1, in2;\n" +
+				"	output out, out1, out2;\n" +
+				"				\n" +
+				"	assign out = in;\n" +
+				"				\n" +
+				"	specify\n" +
+				"		(in => out) = (600,600);\n" +
+				"		(in +=> out) = (600,600);\n" +
+				"		(in -=> out) = (600,600);\n" +
+				"		(in1,in2  *> out      ) = (600,600); // multi-to-single\n" +
+				"		(in1,in2  *> (out-:out2)) = (600,600); // multi-to-range\n" +
+				"		(in1,in2  *> (out+:out2)) = (600,600); // multi-to-range\n" +
+				"		(in1,in2  *> (out :out2)) = (600,600); // multi-to-range\n" +
+				"		(in      -*> out1,out2) = (600,600); // single-to-multi\n" +
+				"		(in1,in2 +*> out1,out2) = (600,600); //multi-to-multi\n" +
+				"	endspecify\n" +
 				"endmodule\n"
 				;
 		SVCorePlugin.getDefault().enableDebug(false);
