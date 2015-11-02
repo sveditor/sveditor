ed88b7241fb6189d9131d8316af084074e394b51
stevenaz <steven.dawson.az@gmail.com>
2015-11-02 16:34:00 -0700

    #411 Source formatting issue in SVEditor 1.8.3

--- sveditor/plugins/net.sf.sveditor.core.tests/src/net/sf/sveditor/core/tests/indent/TestIndentConstraints.java
+++ sveditor/plugins/net.sf.sveditor.core.tests/src/net/sf/sveditor/core/tests/indent/TestIndentConstraints.java
@@ -30,4 +30,49 @@ public class TestIndentConstraints extends SVCoreTestCaseBase {
 		SVCorePlugin.getDefault().enableDebug(false);
 		IndentTests.runTest(getName(), fLog, doc);
 	}
+	public void testIndentInsideConstraint() {
+		String doc = 
+			"class top;\n" +
+			"	int a, b; \n" +
+			"	constraint c_con {\n" +
+			"		\n" +
+			"		if(a == b) {\n" +
+			"			a inside {1,3}; \n" +
+			"		} \n" +
+			"		else {\n" +
+			"			a inside {0,2}; \n" +
+			"		}\n" +
+			"	}\n" +
+			"endclass\n"
+			;
+		
+		SVCorePlugin.getDefault().enableDebug(false);
+		IndentTests.runTest(getName(), fLog, doc);
+	}
+	public void testIndentForeachConstraint() {
+		String doc = 
+			"class c extends uvm_sequence_item; //base input transaction\n" +
+			"	constraint legal_values {\n" +
+			"		// constraint datagram_commads \n" +
+			"		foreach(datagram_commands[j]) \n" +
+			"			{\n" +
+			"			datagram_commands[j] <= 8'h14;\n" +
+			"			if (datagram_commands[j]==DTGRAM_OP_NOP) {\n" +
+			"				//datagram_payload[j] inside {0};   //the value or number of databytes is zero not the number of elements\n" +
+			"				//datagram_bytelength_payload[j] == 0;\n" +
+			"			}\n" +
+			"			if (datagram_commands[j]==DTGRAM_OP_APRD) {\n" +
+			"				//datagram_bytelength_payload[j] inside {[0:1486]};   \n" +
+			"			}\n" +
+			"		} //end datagram_commands constraint\n" +
+		    "\n" +
+			"	} //end legal values constraint\n" +
+		    "\n" +
+			"endclass : c\n"
+		    ;
+	
+		
+		SVCorePlugin.getDefault().enableDebug(false);
+		IndentTests.runTest(getName(), fLog, doc);
+	}
 }
