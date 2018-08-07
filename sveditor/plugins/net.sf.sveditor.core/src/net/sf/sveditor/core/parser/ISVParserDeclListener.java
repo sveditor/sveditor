5ee662bab3adb4b20a8e763158764adcee44b2bd
Matthew Ballance <matt.ballance@gmail.com>
2018-08-06 18:02:08 -0700

    Unsure why these files are marked as changed, but they are...

--- sveditor/plugins/net.sf.sveditor.core/src/net/sf/sveditor/core/parser/ISVParserDeclListener.java
+++ sveditor/plugins/net.sf.sveditor.core/src/net/sf/sveditor/core/parser/ISVParserDeclListener.java
@@ -2,9 +2,11 @@ package net.sf.sveditor.core.parser;
 
 import net.sf.sveditor.core.db.ISVDBItemBase;
 
-public interface ISVParserTypeListener {
+public interface ISVParserDeclListener {
 	
 	void enter_type_scope(ISVDBItemBase item);
+	
+	void declaration(ISVDBItemBase item);
 	
 	void leave_type_scope(ISVDBItemBase item);
 
